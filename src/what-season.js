9f933f0ebf8c24fb709c965cadeddc6a00d2b611
khvorosttt <khvorosttt@yandex.by>
2023-12-15 22:12:31 +0300

    feat: add method implementation detSeason

--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  if(!date) {
+    return 'Unable to determine the time of year!';
+  }
+  if(!(date instanceof Date) || Object.keys(date).length) {
+    throw new Error('Invalid date!');
+  }
+  const month = date.getMonth();
+  if(month >= 0 && month < 2 || month === 11) {
+    return 'winter';
+  } else if (month > 1 && month < 5) {
+    return 'spring';
+  } else if (month > 4 && month < 8) {
+    return 'summer';
+  } else {
+    return 'autumn';
+  }
 }
 
 module.exports = {
