6714e7ee63764282c3fdc93b9ef0b12f0f6ebaf4
khvorosttt <khvorosttt@yandex.by>
2023-12-16 21:59:47 +0300

    feat: add method implementation VigenereCipheringMachine

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,4 +1,7 @@
 const { NotImplementedError } = require('../extensions/index.js');
+const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+                   'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+                    'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
@@ -20,13 +23,83 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
+  constructor(right) {
+    if(right !== undefined) {
+      this.right = right;
+    } else {
+      this.right = true;
+    }
+  }
   encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let encryptStr = '';
+    if(arguments.length < 2 || arguments[0] === undefined || arguments[1] === undefined) {
+      throw new Error('Incorrect arguments!');
+    }
+    let initStr = arguments[0].toUpperCase();
+    let str = initStr.replace(/[.,\/#@|!$%\^&\*;:{}=\-_`~()]/g,"").replace(/\d+/g,'').replaceAll(" ","").toUpperCase();
+    let key = arguments[1].toUpperCase();
+    if(str.length > key.length) {
+      let wholeRep = Math.floor(str.length / key.length);
+      key = key.repeat(wholeRep);
+      let modRep = str.length % key.length;
+      key = key + key.substring(0, modRep);
+    }
+    let j = 0;
+    for(let i = 0; i < str.length; i++) {
+      let indexStr = alphabet.indexOf(str[i]);
+      let indexKey = alphabet.indexOf(key[i]);
+      let indexInitStr = alphabet.indexOf(initStr[j]);
+      while(indexInitStr === -1 && j < initStr.length) {
+        encryptStr += initStr[j];
+        j++;
+        indexInitStr = alphabet.indexOf(initStr[j]);
+      }
+      let index = (indexStr + indexKey) % alphabet.length;
+      encryptStr += alphabet[index];
+      j++;
+    }
+    if(initStr.length > encryptStr.length){
+      encryptStr += initStr.substring(encryptStr.length);
+    }
+    return this.right ? encryptStr : encryptStr.split('').reverse().join('');
   }
   decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let decryptStr = '';
+    if(arguments.length < 2 || arguments[0] === undefined || arguments[1] === undefined) {
+      throw new Error('Incorrect arguments!');
+    }
+    let initStr = arguments[0].toUpperCase();
+    let str = initStr.replace(/[.,\/#@|!$%\^&\*;:{}=\-_`~()]/g,"").replace(/\d+/g,'').replaceAll(" ","").toUpperCase();
+    let key = arguments[1].toUpperCase();
+    if(str.length > key.length) {
+      let wholeRep = Math.floor(str.length / key.length);
+      key = key.repeat(wholeRep);
+      let modRep = str.length % key.length;
+      key = key + key.substring(0, modRep);
+    }
+    let j = 0;
+    for(let i = 0; i < str.length; i++) {
+      let indexStr = alphabet.indexOf(str[i]);
+      let indexKey = alphabet.indexOf(key[i]);
+      let indexInitStr = alphabet.indexOf(initStr[j]);
+      while(indexInitStr === -1 && j < initStr.length) {
+        decryptStr += initStr[j];
+        j++;
+        indexInitStr = alphabet.indexOf(initStr[j]);
+      }
+      let index;
+      if(indexStr >= indexKey) {
+        index = (indexStr - indexKey) % alphabet.length;
+      } else {
+        index = (indexStr - indexKey + alphabet.length) % alphabet.length;
+      }
+      decryptStr += alphabet[index];
+      j++;
+    }
+    if(initStr.length > decryptStr.length){
+      decryptStr += initStr.substring(decryptStr.length);
+    }
+    return this.right ? decryptStr : decryptStr.split('').reverse().join('');
   }
 }
 
