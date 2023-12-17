3478710f31b696f1d01a574f515b1d108c49f2a9
khvorosttt <khvorosttt@yandex.by>
2023-12-18 00:32:31 +0300

    feat: add method implementation renameFiles

--- src/file-names.js
+++ src/file-names.js
@@ -15,9 +15,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  let newNames = [];
+  for(let i = 0; i < names.length; i++) {
+    if(newNames.includes(names[i])) {
+      let count = 0;
+      for(let j = 0; j < i; j++) {
+        if(names[i] === names[j]) {
+          count++;
+        }
+      }
+      if(count === 0) {
+          newNames.push(names[i]+'(' + (count + 1) + ')');
+      }
+      else if(count === 1) {
+        newNames.push(names[i]+'(' + (count) + ')');
+      } else {
+        newNames.push(names[i] +'(' + (count) + ')');
+      }
+      console.log()
+    } else {
+      newNames.push(names[i]);
+    }
+  }
+  return newNames;
 }
 
 module.exports = {
